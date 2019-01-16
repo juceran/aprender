ec7729c8cb5124105aa7785ed318ebd9347baa5f
Juceran Cavalcante <juceran@gmail.com>
2019-01-16 17:37:44 -0300

    Aula121 Exercicio Resolvido

--- aprender/Program.cs
+++ aprender/Program.cs
@@ -9,7 +9,40 @@ namespace aprender
     {
         static void Main(string[] args)
         {
-            Aula118();
+            Aula121(); //exercicio resolvido
+            //Aula118(); //aula118 à aula120
+        }
+
+        private static void Aula121()
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome");
+            Post p1 = new Post(
+                DateTime.Parse("16/01/2019 16:01:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+                );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the force to be with you");
+
+            Post p2 = new Post(
+                  DateTime.Parse("16/01/2019 16:07:21"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
         }
 
         private static void Aula118()
